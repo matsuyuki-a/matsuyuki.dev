28aac38d1f5c420d4ca845e2a144b89c256bce55
rmn <matsuyuki.eca@gmail.com>
2012-11-18 01:25:16 +0900

    add Game Over

--- js/init.js
+++ js/init.js
@@ -1,4 +1,5 @@
 var init = function(){
+  gameState = State.INIT;
   canvas = $( "screen" );
   ctx = canvas.getContext( "2d" );
   width = canvas.getAttribute( "width" );
@@ -14,23 +15,37 @@ var init = function(){
 
   // 円
   var circleSd = new b2CircleDef();
-  circleSd.density = 1.0;
-  circleSd.radius = 20;
+  circleSd.density = 1.0;     // 密度
+  circleSd.radius = 20;       // 半径
   circleSd.restitution = 0;   // 弾性
   circleSd.friction = 0;      // 摩擦
   var circleBd = new b2BodyDef();
-  circleBd.AddShape(circleSd);
-  circleBd.position.Set(100,100);
+  circleBd.AddShape( circleSd );
+  circleBd.position.Set( 100, 100 );
   player = world.CreateBody(circleBd);
+
   // 床
   var groundSd = new b2BoxDef();
-  groundSd.extents.Set(2000, 50);
+  groundSd.extents.Set( 2000, 50 );
   groundSd.restitution = 0; 
   var groundBd = new b2BodyDef();
-  groundBd.AddShape(groundSd);
-  groundBd.position.Set(-500, 500);
-  ground = world.CreateBody(groundBd);
+  groundBd.AddShape( groundSd );
+  groundBd.position.Set( -500, 500 );
+  ground = world.CreateBody( groundBd );
+  
+  // ブロック落下テスト
+  var blockSd = new b2BoxDef();
+  blockSd.density = 100.0;
+  blockSd.extents.Set( 30, 30 );
+  blockSd.restitution = 0.2; // 反発力
+  var blockBd = new b2BodyDef();
+  blockBd.AddShape( blockSd );
+  blockBd.position.Set( 600, -400 );
+  setTimeout( function(){
+    block[ 0 ] = world.CreateBody( blockBd );
+  }, 2500 );
 
+  gameState = State.USER;
   setTimeout( step, 10 );
 };
 
--- js/step.js
+++ js/step.js
@@ -1,44 +1,62 @@
 var step = function(){
-  ctx.fillRect( 0, 0, width, height );
-
   keyStore.update();
+  switch( gameState ){
+    case State.USER: 
+      // 接触判定
+      var onGround = false;
+      for( var i = player.m_contactList; i; i = i.next ){
+        // 一般のアクションゲームだとこの判定では弱い. 今回は床が下に1つなのでこれでOK.
+        if( i.other == ground ){ // 接地判定
+          onGround = true;
+          break;
+        }else if( i.other == block[ 0 ] ){ // 死亡当たり判定 (仮)
+          gameState = State.GAMEOVER;
+          break;
+        }
+      }
 
-  // 接地判定
-  var onGround = false;
-  for( var i = player.m_contactList; i; i = i.next ){
-    // 一般のアクションゲームだとこの判定では弱い. 今回は床が下に1つなのでこれでOK.
-    if( i.other == ground ){ 
-      onGround = true;
+      // 左右移動
+      var v = player.GetLinearVelocity();
+      if( keyStore.state[ 37 ] == KeyState.DOWN || keyStore.state[ 37 ] == KeyState.PRESS ){
+        player.WakeUp();
+        v.x = -100;
+      }else if( keyStore.state[ 39 ] == KeyState.DOWN || keyStore.state[ 39 ] == KeyState.PRESS ){
+        player.WakeUp();
+        v.x =  100;
+      }else{
+        v.x = 0;
+      }
+
+      // ジャンプ
+      if( onGround && keyStore.state[ 90 ] == KeyState.DOWN ){
+        player.WakeUp();
+        v.y -= 300;
+      }
+
+      // 速度の確定
+      player.SetLinearVelocity( new b2Vec2( v.x, v.y ) );
+
+      world.Step( 1.0 / 60, 1 );
       break;
-    }
+    case State.GAMEOVER:
+
+
+      break;
   }
 
-  var v = player.GetLinearVelocity();
-  var vx = v.x, vy = v.y;
-
-  if( keyStore.state[ 37 ] == KeyState.DOWN || keyStore.state[ 37 ] == KeyState.PRESS ){
-    player.WakeUp();
-    vx = -100;
-  }else if( keyStore.state[ 39 ] == KeyState.DOWN || keyStore.state[ 39 ] == KeyState.PRESS ){
-    player.WakeUp();
-    vx =  100;
-  }else{
-    vx = 0;
-  }
-
-  // JUMP
-  if( onGround && keyStore.state[ 90 ] == KeyState.DOWN ){
-    player.WakeUp();
-    vy -= 300;
-  }
-
-  player.SetLinearVelocity( new b2Vec2( vx, vy ) );
-
-  world.Step( 1.0 / 60, 1 );
+  // 描画
+  var camera = {
+    x: player.m_position.x -  width / 2,
+    y: player.m_position.y - height * 2 / 3
+  };
   ctx.clearRect( 0, 0, width, height );
   ctx.fillStyle = "#000000";
   ctx.fillRect( 0, 0, width, height );
-  drawWorld( world, ctx );
+  drawWorld( world, ctx, camera.x, camera.y );
+  if( gameState == State.GAMEOVER ){
+    ctx.fillStyle = "rgba( 255, 0, 0, 0.3 )";
+    ctx.fillRect( 0, 0, width, height );
+  }
   /*
      for( var b = world.m_bodyList; b; b = b.m_next ){
      ctx.fillColor = "#FFFFFF";
@@ -47,5 +65,6 @@ var step = function(){
      ctx.fill();
      }
      */
+
   setTimeout( step, 10 );
 };
