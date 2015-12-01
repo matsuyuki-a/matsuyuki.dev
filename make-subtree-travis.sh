f7dc528bd8f795e5407a868687d76a684e92c43b
Matsuyuki(rmn_e) <matsuyuki.eca@gmail.com>
2015-12-01 23:23:29 +0900

    ...

--- make-subtree-travis.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-if [ -e "hugo/public" ]; then
-  git rm -rf hugo/public
-  git commit -m "temporal remove public"
-  git subtree add --prefix=hugo/public https://$GH_TOKEN@github.com/rmn31415/rmn31415.github.io.git gh-pages --squash
-
-  git push origin master
-fi
