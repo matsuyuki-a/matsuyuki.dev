010dd47f2e5c915584bad5cbcba43c1a7e9fa53f
Matsuyuki(rmn_e) <matsuyuki.eca@gmail.com>
2015-12-01 20:44:04 +0900

    make-subtree.shのsyntaxerror修正

--- make-subtree.sh
+++ make-subtree.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-if [ -e "hugo/public" ] then
+if [ -e "hugo/public" ]; then
   git rm -rf hugo/public
   git commit -m "temporal remove public"
   git subtree add --prefix=hugo/public git@github.com:rmn31415/news.rmn-web.net.git gh-pages --squash
